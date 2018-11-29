66976ae0e915573d4668ecc7dcdefcd86a513d68
BestXiaosong <306027376@qq.com>
2018-11-29 18:04:28 +0800

    接口统一返回方法写入

--- config/base.php
+++ config/base.php
@@ -7,4 +7,5 @@ return [
     'ADMIN_URL' => env('ADMIN_URL', 'admin'),
     'API_URL'   => env('API_URL', 'api'),
     'INDEX_URL' => env('INDEX_URL', 'url'),
+    'var_ajax' => env('var_ajax', '_ajax'),
 ];
