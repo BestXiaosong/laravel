2bbac5c7e1c0b0682bb0761cc86326c0bd64bb44
BestXiaosong <306027376@qq.com>
2018-11-29 14:39:44 +0800

    接口统一返回方法写入

--- app/Helpers/functions.php
+++ app/Helpers/functions.php
@@ -16,3 +16,20 @@ function api_return($code,$msg = '',$data = [],$headers = []){
     throw new \Illuminate\Http\Exceptions\HttpResponseException($response);
 }
 
+/**
+ * Created by xiaosong
+ * E-mail:4155433@gmail.com
+ * 手动拦截异常返回
+ */
+function error_exception($code,$msg = '',$data = [],$headers = []){
+    $content['code']   = $code;
+    $content['msg']    = $msg;
+    $content['data']   = $data;
+    $content['time']   = request()->server('REQUEST_TIME');
+
+    $response = \Illuminate\Http\Response::create($content,200,$headers);
+    return $response;
+}
+
+
+
